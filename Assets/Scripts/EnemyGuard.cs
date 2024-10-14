ce78c01a27e3083cc2a380c36fcc54b7f14d57ec
Jazmine <jazmine.chargualaf@gmail.com>
2024-10-13 23:39:11 -0600

    Update to Guard AI
    - Guard uses NavMesh to track player after detecting them
    - Guard returns to patrolling waypoints when they lose sight of player

--- Assets/Scripts/EnemyGuard.cs
+++ Assets/Scripts/EnemyGuard.cs
@@ -8,42 +8,65 @@ using UnityEngine.AI;
 
 public class EnemyGuard : MonoBehaviour
 {
-    private Coroutine chaseCoroutine;
+    #region Coroutines and Flags
+    // Coroutines
+    private Coroutine detectingCoroutine;
+    private Coroutine stopDetectCoroutine;
 
-    //Mesh renderer colors
-    private Color originalColor = new(0.0f,0.0f,0.0f,0.0f);
+    // Flags
+    private bool playerInSight;
+    private bool playerDetected;
+    private bool isDetectingPlayer;
+    private bool isWaiting = false;
+    public bool playerVisible;
+
+    #endregion
+
+    #region Detection Settings
+    [Header("Detection Settings")]
+    [SerializeField] private float detectionTime = 3.0f;
+    [SerializeField] private FieldOfViewDetector fieldOfView;
+    [SerializeField] private float mainFOVAngle;
+    [SerializeField] private float peripheralFOVAngle = 180.0f;
+    [SerializeField] private float detectionRadius = 5.0f;
+    [SerializeField] private float shoulderDetectionRadius = 2.0f;
+
+    private float playerVisibleTimer;
+    private Vector3 directionToPlayer;
+    private float distanceToPlayer;
+    private float angleToPlayer;
+    #endregion
+
+    #region UI and Visuals
+    [Header("UI and Visuals")]
+    // UI elements
+    [SerializeField] private GameObject alertUIObject;
+    [SerializeField] private GameObject suspiciousUIObject;
+
+    // Mesh renderer colors
+    private Color originalColor = new(0.0f, 0.0f, 0.0f, 0.0f);
     private Color detectedColor = new(0.5f, 0.0f, 0.0f, 0.5f);
 
-    //public event Action OnGuardCaughtPlayer;
+    #endregion
 
-    [Header("Guard Variables")]
-    [SerializeField] private float detectionTime = 3.0f;
-    private float originalDetectionTime;
-    private float playerVisibleTimer;
-    [SerializeField] GameObject alertUIObject;
-    [SerializeField] GameObject suspiciousUIObject;
+    #region Movement and Pathing
+    [Header("Movement and Pathing")]
     private NavMeshAgent agent;
 
-
-    [Header("Pathing Variables")]
-    private Vector3[] waypoints;
+    // Pathing variables
     [SerializeField] private Transform pathHolder;
     [SerializeField] private float speed = 3.0f;
     [SerializeField] private float waitTime = 3.0f;
     [SerializeField] private float turnSpeed = 90.0f;
+    private Vector3[] waypoints;
+    private Vector3 targetWaypoint;
+    private int targetWaypointIndex = 0;
+    #endregion
 
+    #region Player Reference
     private GameObject player;
     private Transform playerTransform;
-    private Vector3 directionToPlayer;
-    private float angleToPlayer;
-    private bool playerDetected = false;
-
-    [SerializeField] private FieldOfViewDetector fieldOfView;
-    private float distanceToPlayer;
-    [SerializeField] private float mainFOVAngle;
-    [SerializeField] private float peripheralFOVAngle = 120.0f;
-    [SerializeField] private float detectionRadius = 5.0f;
-    [SerializeField] private float shoulderDetectionRadius = 2.0f;
+    #endregion
 
 
     void Start()
@@ -66,9 +89,6 @@ public class EnemyGuard : MonoBehaviour
         // Set the detection radius of the sphere collider
         transform.GetComponent<SphereCollider>().radius = detectionRadius;
 
-        // Save the original detection time
-        originalDetectionTime = detectionTime;
-
         // Get the waypoints from the pathHolder
         waypoints = new Vector3[pathHolder.childCount];
         for (int i = 0; i < waypoints.Length; i++)
@@ -85,8 +105,25 @@ public class EnemyGuard : MonoBehaviour
         alertUIObject.SetActive(false);
         suspiciousUIObject.SetActive(false);
 
-        // Start following path
-        StartCoroutine(FollowPath(waypoints));
+        // Set the first waypoint as the target
+        targetWaypoint = waypoints[targetWaypointIndex];
+        agent.SetDestination(targetWaypoint);
+
+    }
+
+    void Update()
+    {
+        CheckPlayerVisibility();
+
+        if(!playerDetected && !playerInSight)
+        {
+            FollowPath();
+        }
+        else if (playerDetected && playerInSight)
+        {
+            agent.SetDestination(playerTransform.position);
+        }
+
     }
 
     private void CheckPlayerVisibility()
@@ -97,17 +134,13 @@ public class EnemyGuard : MonoBehaviour
         angleToPlayer = Vector3.Angle(transform.forward, directionToPlayer);
 
         // Reset player detection
-        playerDetected = false;
+        playerVisible = false;
 
         // Check if player is within detection radius
         if (distanceToPlayer <= detectionRadius)
         {
             // If player is in line of sight
-            RaycastHit hit;
-
-            Debug.DrawRay(transform.position, directionToPlayer, Color.red);
-
-            if (Physics.Raycast(transform.position, directionToPlayer, out hit, detectionRadius))
+            if (Physics.Raycast(transform.position, directionToPlayer, out RaycastHit hit, detectionRadius))
             {
                 if (hit.collider.CompareTag("Player"))
                 {
@@ -116,167 +149,168 @@ public class EnemyGuard : MonoBehaviour
                     {
                         Debug.Log("Player in Main FOV");
 
-                        detectionTime = originalDetectionTime;
-                        StartChaseCoroutine(detectionTime);
-                        playerDetected = true;
-
-                        alertUIObject.SetActive(true);
-                        suspiciousUIObject.SetActive(false);
+                        StartDetecting(detectionTime - 2.0f);
+                        playerVisible = true;
                     }
                     // If player is within peripheral view
                     else if (angleToPlayer <= peripheralFOVAngle / 2.0f)
                     {
                         Debug.Log("Player in Peripheral FOV");
 
-                        detectionTime = originalDetectionTime + 1.0f;
-                        StartChaseCoroutine(detectionTime);
-                        playerDetected = true;
+                        StartDetecting(detectionTime);
+                        playerVisible = true;
 
-                        suspiciousUIObject.SetActive(true);
-                        alertUIObject.SetActive(false);
                     }
                     // If player is behind the enemy (over the shoulder) and closer than the detection radius
                     else if (distanceToPlayer <= shoulderDetectionRadius)
                     {
                         Debug.Log("Player in Shoulder FOV");
-
-                        detectionTime = originalDetectionTime / 2.0f;
-                        StartChaseCoroutine(detectionTime);
-                        playerDetected = true;
-
-                        suspiciousUIObject.SetActive(true);
-                        alertUIObject.SetActive(false);
+                        StartDetecting(detectionTime / 2.0f);
+                        playerVisible = true;
                     }
                 }
             }
         }
 
-        if (!playerDetected)
+        if (!playerVisible)
+        { 
+            StopDetecting(detectionTime);
+
+            playerInSight = false;
+            playerDetected = false;
+        }
+        
+    }
+
+    #region Player Detection Functions
+    private void StartDetecting(float detectionTime)
+    {
+        // Cancel any existing stop detection coroutine
+
+        if (stopDetectCoroutine != null)
         {
-            StopChaseCoroutine(detectionTime);
+            StopCoroutine(stopDetectCoroutine);
+            stopDetectCoroutine = null;
+        }
+
+        // Start detecting the player if not already detecting
+        if (!isDetectingPlayer && detectingCoroutine == null)
+        {
+            isDetectingPlayer = true;
+            detectingCoroutine = StartCoroutine(Detect(detectionTime));
         }
     }
 
-
-    private void OnTriggerStay(Collider other)
+    private void StopDetecting(float detectionTime)
     {
-        if (other.CompareTag("Player"))
+        // Cancel any existing stop detection coroutine
+        if (stopDetectCoroutine != null)
         {
-            CheckPlayerVisibility();
+            StopCoroutine(stopDetectCoroutine);
+            stopDetectCoroutine = null;
         }
+        // Stop detecting the player
+        if (detectingCoroutine != null)
+        {
+            StopCoroutine(detectingCoroutine);
+            detectingCoroutine = null;
+        }
+
+        // Reset detection variables
+        playerDetected = false;
+        isDetectingPlayer = false;
+
+        // Start the stop detection coroutine
+        stopDetectCoroutine = StartCoroutine(StopDetect(detectionTime));
+
     }
 
-    private void OnTriggerExit(Collider other)
+    IEnumerator Detect(float detectionTime)
     {
-        if (other.CompareTag("Player"))
-        {
-            Debug.Log("Player exited detection radius");
+        Debug.Log("Detecting player");
 
-            StopChaseCoroutine(detectionTime);
-        }
-    }
-
-    private void StartChaseCoroutine(float detectionTime)
-    {
-        if (chaseCoroutine == null)
-        {
-            chaseCoroutine = StartCoroutine(Chase(detectionTime));
-        }
-    }
-
-    private void StopChaseCoroutine(float detectionTime)
-    {
-        if (chaseCoroutine != null)
-        {
-            StopCoroutine(chaseCoroutine);
-            chaseCoroutine = null;
-            StartCoroutine(StopChase(detectionTime));
-        }
-    }
-    IEnumerator FollowPath(Vector3[] waypoints)
-    {
-        agent.SetDestination(waypoints[0]);
-        int targetWaypointIndex = 1;
-        Vector3 targetWaypoint = waypoints[targetWaypointIndex];
-
-
-        while(true)
-        {
-             agent.SetDestination(targetWaypoint);
-
-            if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
-            {
-                targetWaypointIndex = (targetWaypointIndex + 1) % waypoints.Length;
-                targetWaypoint = waypoints[targetWaypointIndex];
-
-                yield return new WaitForSeconds(waitTime);
-
-                agent.SetDestination(targetWaypoint);
-            }
-            yield return null;
-            }
-        }
-
-    IEnumerator TurnToFace(Vector3 lookTarget)
-    {
-        Vector3 direction = (lookTarget - transform.position).normalized;
-        float targetAngle = 90 - Mathf.Atan2(direction.z, direction.x) * Mathf.Rad2Deg;
-
-        while(Mathf.Abs(Mathf.DeltaAngle(transform.eulerAngles.y, targetAngle)) > 0.05f)
-        {
-            float angle = Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetAngle, turnSpeed * Time.deltaTime);
-            transform.eulerAngles = Vector3.up * angle;
-            yield return null;
-        }
-    }
-
-    IEnumerator Chase(float detectionTime)
-    {
         while (playerVisibleTimer < detectionTime)
         {
+            suspiciousUIObject.SetActive(true);
+            suspiciousUIObject.transform.LookAt(Camera.main.transform);
+            Debug.Log("UI Object LookAt Camera");
+
             playerVisibleTimer += Time.deltaTime;
             playerVisibleTimer = Mathf.Clamp(playerVisibleTimer, 0, detectionTime);
 
-            if(alertUIObject.activeSelf)
-            {
-                alertUIObject.transform.LookAt(Camera.main.transform);
-            }
-            if (suspiciousUIObject.activeSelf)
-            {
-                suspiciousUIObject.transform.LookAt(Camera.main.transform);
-            }
-
-            // Interpolate the mesh renderer's original color to detected color based on player detection
             fieldOfView.GetComponent<MeshRenderer>().materials[0].color = Color.Lerp(originalColor, detectedColor, playerVisibleTimer / detectionTime);
 
-            if (playerVisibleTimer >= detectionTime)
-            {
-                // TODO: perhaps have an event to specifically handle OnGuardCaughtPlayer instead of ending the game
-                GameManager.instance.YouLose();
-                yield break;
-                //OnGuardCaughtPlayer?.Invoke();
-            }
             yield return null;
+
         }
+        if (playerVisibleTimer >= detectionTime)
+        {
+            Debug.Log("Player detected");
+            playerDetected = true;
+            playerInSight = true;
+            alertUIObject.SetActive(true);
+            suspiciousUIObject.SetActive(false);
+        }
+
+         isDetectingPlayer = false;
     }
 
-    IEnumerator StopChase(float detectionTime)
+    IEnumerator StopDetect(float detectionTime)
     {
+        Debug.Log("Stopping detection");
+
         while (playerVisibleTimer > 0)
         {
             playerVisibleTimer -= Time.deltaTime;
             playerVisibleTimer = Mathf.Clamp(playerVisibleTimer, 0, detectionTime);
 
             alertUIObject.SetActive(false);
-            suspiciousUIObject.SetActive(false);
+            suspiciousUIObject.SetActive(true);
 
-            // Interpolate the mesh renderer's detected color back to original color based on player detection
+            // Interpolate the mesh renderer's detected color back to original color based on player detection time
             fieldOfView.GetComponent<MeshRenderer>().materials[0].color = Color.Lerp(detectedColor, originalColor, 1 - (playerVisibleTimer / detectionTime));
+            yield return null;
         }
-        yield return null;
+
+        suspiciousUIObject.SetActive(false);
+        playerDetected = false;
+        stopDetectCoroutine = null;
     }
 
+    #endregion
 
+
+    #region Pathing Functions
+
+    void FollowPath()
+    {
+        // If agent reaches its current waypoint
+        if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+        {
+            if (!isWaiting)
+            {
+                StartCoroutine(WaitAtWaypoint());
+            }
+        }
+    }
+
+    IEnumerator WaitAtWaypoint()
+    {
+        isWaiting = true;
+        yield return new WaitForSeconds(waitTime);  // Wait for the defined wait time at the waypoint
+
+        // Move to the next waypoint
+        targetWaypointIndex = (targetWaypointIndex + 1) % waypoints.Length;
+        targetWaypoint = waypoints[targetWaypointIndex];
+        agent.SetDestination(targetWaypoint);
+
+        isWaiting = false;
+    }
+
+    #endregion
+
+
+    #region Gizmos
     void OnDrawGizmos()
     {
         Vector3 startPosition = pathHolder.GetChild(0).position;
@@ -295,5 +329,6 @@ public class EnemyGuard : MonoBehaviour
         //Gizmos.color = Color.blue;
         //Gizmos.DrawWireSphere(transform.position, shoulderDetectionRadius * transform.lossyScale.x ); // Draw the shoulder detection radius
     }
+    #endregion
 }
 
